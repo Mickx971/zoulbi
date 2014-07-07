1dba47ae087fbc6cdc050a9760077027971a5fb5
arnauve <yehouda_arnauve@hotmail.com>
2014-07-07 14:42:22 +0200

    .bat execute

--- zoulbi-Yehouda/calc.bat
+++ zoulbi-Yehouda/calc.bat
@@ -14,11 +14,12 @@ rename zoulbi.tab.h zoulbi.h
 
 flex zoulbi.l
 
-
 gcc -c lex.yy.c -o zoulbi.lex.o
 gcc -c zoulbi.tab.c -o zoulbi.y.o
 gcc -c makeTree.c -o makeTree.o
-gcc -o zoulbi zoulbi.lex.o zoulbi.y.o makeTree.o
+gcc -c evalTree.c -o evalTree.o
+gcc -c utils.c -o utils.o
+gcc -o zoulbi zoulbi.lex.o zoulbi.y.o makeTree.o evalTree.o utils.o
 
 zoulbi -f in.txt
 
